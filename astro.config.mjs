46835472b7e6387cdfa120c6e999e605b9662ca6
Pedro Alejandro <alejandroleonpedro7@gmail.com>
2025-08-03 02:30:44 -0500

    primer avance

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,26 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';     
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://accessibletravelperu.com', 
+
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [
+    sitemap() 
+  ],
+
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'es', 'fr'],
+    routing: {
+      prefixDefaultLocale: true,
+      redirectToDefaultLocale: true
+    }
+  }
+});
